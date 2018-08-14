9c90f9c50531187b56ffbb75206cb1f3ceeb52c7
abhi5h3k <abhishekbhardwajca@gmail.com>
2018-08-14 22:39:48 +0530

    Initial commit

---
+++ app/src/main/java/com/news/abhishek/newsapp/SplashScreen.java
@@ -0,0 +1,13 @@
+package com.news.abhishek.newsapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
